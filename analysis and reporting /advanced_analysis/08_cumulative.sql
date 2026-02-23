fb40def988e2217ad9a2bde84cb83839649c61ce
Mennah Elsheikh <124211281+Mennah-Elsheikh@users.noreply.github.com>
2026-02-24 00:42:32 +0200

    Update cumulative analysis SQL for sales performance
    Refactor cumulative analysis SQL to include running totals and moving averages.

--- analysis and reporting /advanced_analysis/08_cumulative.sql
+++ analysis and reporting /advanced_analysis/08_cumulative.sql
@@ -1,49 +1,31 @@
 /*
 ===============================================================================
-Change Over Time Analysis
+Cumulative Analysis
 ===============================================================================
 Purpose:
-    - To track trends, growth, and changes in key metrics over time.
-    - For time-series analysis and identifying seasonality.
-    - To measure growth or decline over specific periods.
+    - To calculate running totals or moving averages for key metrics.
+    - To track performance over time cumulatively.
+    - Useful for growth analysis or identifying long-term trends.
 
 SQL Functions Used:
-    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
-    - Aggregate Functions: SUM(), COUNT(), AVG()
+    - Window Functions: SUM() OVER(), AVG() OVER()
 ===============================================================================
 */
 
--- Analyse sales performance over time
--- Quick Date Functions
+-- Calculate the total sales per month 
+-- and the running total of sales over time 
 SELECT
-    YEAR(order_date) AS order_year,
-    MONTH(order_date) AS order_month,
-    SUM(sales_amount) AS total_sales,
-    COUNT(DISTINCT customer_key) AS total_customers,
-    SUM(quantity) AS total_quantity
-FROM gold.fact_sales
-WHERE order_date IS NOT NULL
-GROUP BY YEAR(order_date), MONTH(order_date)
-ORDER BY YEAR(order_date), MONTH(order_date);
-
--- DATETRUNC()
-SELECT
-    DATETRUNC(month, order_date) AS order_date,
-    SUM(sales_amount) AS total_sales,
-    COUNT(DISTINCT customer_key) AS total_customers,
-    SUM(quantity) AS total_quantity
-FROM gold.fact_sales
-WHERE order_date IS NOT NULL
-GROUP BY DATETRUNC(month, order_date)
-ORDER BY DATETRUNC(month, order_date);
-
--- FORMAT()
-SELECT
-    FORMAT(order_date, 'yyyy-MMM') AS order_date,
-    SUM(sales_amount) AS total_sales,
-    COUNT(DISTINCT customer_key) AS total_customers,
-    SUM(quantity) AS total_quantity
-FROM gold.fact_sales
-WHERE order_date IS NOT NULL
-GROUP BY FORMAT(order_date, 'yyyy-MMM')
-ORDER BY FORMAT(order_date, 'yyyy-MMM');
+	order_date,
+	total_sales,
+	SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales,
+	AVG(avg_price) OVER (ORDER BY order_date) AS moving_average_price
+FROM
+(
+    SELECT 
+        DATETRUNC(year, order_date) AS order_date,
+        SUM(sales_amount) AS total_sales,
+        AVG(price) AS avg_price
+    FROM gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATETRUNC(year, order_date)
+) t
